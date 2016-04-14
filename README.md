ebd31cbe1a9ea8698d0ec407bd5de07466f2daee
Jonathan Tamsut <jtamsut1993@gmail.com>
2016-04-13 17:18:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# React Boilerplate
+
+Setting up a React.js application can be a tedious process. This
+repository has the basic configuration set up so you can start building a
+React application right away.
+
+## How to Use This
+
+After cloning the repository don't forget to:
+
+```
+npm install
+
+```
+
+## Author
+
+This was created by Jonathan Tamsut.
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
