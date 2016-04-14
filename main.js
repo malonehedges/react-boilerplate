19e8e5394a118e1b18526dbb9068b9ccc4d0d77f
Jonathan Tamsut <jtamsut1993@gmail.com>
2016-04-13 17:31:45 -0700

    test boilerplate

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 var React = require('react');
-var 
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<Hello />, document.getElementById(("main")));
